18a164e404a4fd3927ac7781cdd16efc51294bba
Maria Hidalgo <maria.hidalgo@agile42.com>
2020-06-15 19:03:15 +0200

    Using unique smart pointer

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -2,6 +2,7 @@
 #include <wx/colour.h>
 #include <wx/image.h>
 #include <string>
+#include <memory>
 #include "chatbot.h"
 #include "chatlogic.h"
 #include "chatgui.h"
@@ -118,7 +119,9 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    // _chatLogic = new ChatLogic(); 
+    // with unique smart pointer
+    _chatLogic = std::make_unique<ChatLogic>();
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -135,7 +138,8 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    // deallocate _chatLogic is not necessary in unique smart pointer, it is already on spart pointer
+    // delete _chatLogic;
 
     ////
     //// EOF STUDENT CODE
